aa6a7e2f351b7c1a349b5cfbb53b2469157bd1e7
KokaKiwi <kokakiwi+git@kokakiwi.net>
2025-01-30 10:34:31 +0100

    pkgs.kubo.local: 0.32.1 -> 0.33.0
    Diff: https://github.com/ipfs/kubo/compare/None...v0.33.0

--- pkgs/applications/networking/kubo/default.nix
+++ pkgs/applications/networking/kubo/default.nix
@@ -6,14 +6,14 @@
 }:
 buildGoModule rec {
   pname = "kubo";
-  version = "0.32.1"; # When updating, also check if the repo version changed and adjust repoVersion below
+  version = "0.33.0"; # When updating, also check if the repo version changed and adjust repoVersion below
 
   passthru.repoVersion = "16"; # Also update kubo-migrator when changing the repo version
 
   # Kubo makes changes to its source tarball that don't match the git source.
   src = fetchurl {
     url = "https://github.com/ipfs/kubo/releases/download/v${version}/kubo-source.tar.gz";
-    hash = "sha256-/72omsDZ2+nuPHkZXtR3MSsxWicxC0YnFmKcHF22C+0=";
+    hash = "sha256-TcjxdeVnGdk+pA1MizIZ3UBKy6PWHI1h66/PNWrY2DA=";
   };
 
   # tarball contains multiple files/directories
