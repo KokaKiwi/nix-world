b5b661b2ec7d994776b07669887a669359ef949e
KokaKiwi <kokakiwi+git@kokakiwi.net>
2025-02-05 19:59:17 +0100

    hosts(kiwivault): Stuff

--- hosts/kiwivault/nixos/users/default.nix
+++ hosts/kiwivault/nixos/users/default.nix
@@ -7,8 +7,12 @@
 
   users.groups.media = { };
 
-  cluster.ssh.users = {
-    nixos = { };
-    root = { };
+  cluster.ssh = {
+    keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII7KXVL6BFeXypWcBnvNUl4NJjLDonio5csxT+RgRaCy kokakiwi@KIWI-GAMES-ARCH"
+    ];
+
+    users.nixos = { };
+    users.root = { };
   };
 }
