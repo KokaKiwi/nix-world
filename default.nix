05e843a5e30718ccddf30b558d98f280ad824591
KokaKiwi <kokakiwi+git@kokakiwi.net>
2025-02-06 17:08:40 +0100

    Refactor

--- default.nix
+++ default.nix
@@ -13,13 +13,45 @@ let
       })
     ];
   };
-  lib = import ./lib {
-    inherit pkgs sources;
+  inherit (pkgs) lib;
+
+  mkWorld = hosts: let
+    evalHost = name: host: lib.evalModules {
+      class = "nix-world";
+
+      modules = [
+        ./modules/world
+        {
+          name = lib.mkDefault name;
+        }
+        host
+      ];
+
+      specialArgs = {
+        inherit pkgs sources;
+        lib = lib.extend (self: super: {
+          world = import ./lib self;
+        });
+
+        hosts = hostModules;
+
+        modulesPath = ./modules;
+        pkgsPath = ./pkgs;
+        secretsPath = ./secrets;
+      };
+    };
+
+    hostModules = lib.mapAttrs evalHost hosts;
+    hostPackages = lib.mapAttrs (name: module: module.config.package // {
+      inherit module;
+      inherit (module) config;
+    }) hostModules;
+  in pkgs.linkFarm "world" hostPackages // hostPackages // {
+    hosts = hostModules;
   };
 in {
-  hosts = lib.mkWorld (import ./hosts);
+  hosts = mkWorld (import ./hosts);
 
   inherit sources;
-  inherit (pkgs) pkgs lib;
-  worldLib = lib;
+  inherit pkgs lib;
 }
--- lib/default.nix
+++ lib/default.nix
@@ -1,39 +1,4 @@
-{ pkgs, sources }:
-let
-  inherit (pkgs) lib;
-in {
-  mkWorld = hosts: let
-    evalHost = name: host: lib.evalModules {
-      class = "nix-world";
-
-      modules = [
-        ../modules/world
-        {
-          name = lib.mkDefault name;
-        }
-        host
-      ];
-
-      specialArgs = {
-        inherit pkgs sources;
-        lib = lib.extend (self: super: {
-          world = import ./world self;
-        });
-
-        hosts = hostModules;
-
-        modulesPath = ../modules;
-        pkgsPath = ../pkgs;
-        secretsPath = ../secrets;
-      };
-    };
-
-    hostModules = lib.mapAttrs evalHost hosts;
-    hostPackages = lib.mapAttrs (name: module: module.config.package // {
-      inherit module;
-      inherit (module) config;
-    }) hostModules;
-  in pkgs.linkFarm "world" hostPackages // hostPackages // {
-    hosts = hostModules;
-  };
+lib:
+{
+  types = import ./types.nix lib;
 }
