76743d3dcc92d1868e584bf49f0e3d898c828a6a
KokaKiwi <kokakiwi+git@kokakiwi.net>
2025-02-18 19:07:20 +0100

    pkgs: Import cargo-leptos

---
+++ pkgs/applications/by-name/ca/cargo-leptos/package.nix
@@ -0,0 +1,40 @@
+{
+  darwin,
+  fetchFromGitHub,
+  lib,
+  rustPlatform,
+  stdenv,
+}:
+rustPlatform.buildRustPackage rec {
+  pname = "cargo-leptos";
+  version = "0.2.28";
+
+  src = fetchFromGitHub {
+    owner = "leptos-rs";
+    repo = "cargo-leptos";
+    tag = "v${version}";
+    hash = "sha256-SjpfM963Zux+H5QhK8prvDLuI56fP5PqX5gcVbthRx4=";
+  };
+
+  useFetchCargoVendor = true;
+  cargoHash = "sha256-Da9ei4yAOfhSQmQgrUDZCmMeJXTfGnYhI1+L0JT/ECs=";
+
+  buildInputs = lib.optionals stdenv.hostPlatform.isDarwin (with darwin.apple_sdk.framework; [
+    SystemConfiguration
+    Security
+    CoreServices
+  ]);
+
+  # https://github.com/leptos-rs/cargo-leptos#dependencies
+  buildFeatures = [ "no_downloads" ]; # cargo-leptos will try to install missing dependencies on its own otherwise
+  doCheck = false; # Check phase tries to query crates.io
+
+  meta = with lib; {
+    description = "Build tool for the Leptos web framework";
+    mainProgram = "cargo-leptos";
+    homepage = "https://github.com/leptos-rs/cargo-leptos";
+    changelog = "https://github.com/leptos-rs/cargo-leptos/releases/tag/v${version}";
+    license = with licenses; [ mit ];
+    maintainers = with maintainers; [ benwis ];
+  };
+}
