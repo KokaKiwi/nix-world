4a5f92dbab6d47f8a51f918ea0f4925f16ffa6ee
KokaKiwi <kokakiwi+git@kokakiwi.net>
2025-02-23 13:08:51 +0100

    hosts.kira.home(programs.vscode): Update config

--- hosts/kira/home/programs/vscode.nix
+++ hosts/kira/home/programs/vscode.nix
@@ -8,22 +8,24 @@ in {
       vscode = pkgs.kiwiPackages.vscodium;
     in config.lib.opengl.wrapPackage vscode { };
 
-    extensions = with nix-extensions.vscode-marketplace; [
-      catppuccin.catppuccin-vsc
-      catppuccin.catppuccin-vsc-icons
-      rust-lang.rust-analyzer
-      jnoortheen.nix-ide
-    ];
+    profiles.default = {
+      extensions = with nix-extensions.vscode-marketplace; [
+        catppuccin.catppuccin-vsc
+        catppuccin.catppuccin-vsc-icons
+        rust-lang.rust-analyzer
+        jnoortheen.nix-ide
+      ];
 
-    userSettings = {
-      "files.autoSave" = "off";
-      "[nix]"."editor.tabSize" = 2;
+      userSettings = {
+        "files.autoSave" = "off";
+        "[nix]"."editor.tabSize" = 2;
 
-      "workbench.colorTheme" = "Catppuccin Mocha";
-      "catppuccin.accentColor" = "green";
+        "workbench.colorTheme" = "Catppuccin Mocha";
+        "catppuccin.accentColor" = "green";
 
-      "nix.enableLanguageServer" = true;
-      "nix.serverPath" = "${pkgs.nixd}/bin/nixd";
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "${pkgs.nixd}/bin/nixd";
+      };
     };
   };
 }
