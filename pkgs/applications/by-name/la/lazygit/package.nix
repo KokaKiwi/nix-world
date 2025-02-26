2a32154c15ab00cca0e62c5d3e5cb93681eba6fe
KokaKiwi <kokakiwi+git@kokakiwi.net>
2025-02-26 15:59:16 +0100

    pkgs: Import lazygit

---
+++ pkgs/applications/by-name/la/lazygit/package.nix
@@ -0,0 +1,40 @@
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
+buildGoModule rec {
+  pname = "lazygit";
+  version = "0.47.1";
+
+  src = fetchFromGitHub {
+    owner = "jesseduffield";
+    repo = "lazygit";
+    tag = "v${version}";
+    hash = "sha256-UZG4D2lMw6v1yo4qDHGTuxH7ieyq3k/2hraYaLqjkW4=";
+  };
+
+  vendorHash = null;
+  subPackages = [ "." ];
+
+  ldflags = [
+    "-X main.version=${version}"
+    "-X main.buildSource=nix"
+  ];
+
+  meta = {
+    description = "Simple terminal UI for git commands";
+    homepage = "https://github.com/jesseduffield/lazygit";
+    changelog = "https://github.com/jesseduffield/lazygit/releases/tag/v${version}";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [
+      Br1ght0ne
+      equirosa
+      khaneliman
+      paveloom
+      starsep
+      sigmasquadron
+    ];
+    mainProgram = "lazygit";
+  };
+}
