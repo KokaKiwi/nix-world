361d965d3c77ac554b6559409026bea23a4302a7
KokaKiwi <kokakiwi+git@kokakiwi.net>
2025-02-27 23:50:42 +0100

    pkgs.b3sum.local: 1.6.0 -> 1.6.1
    Diff: https://github.com/BLAKE3-team/BLAKE3/compare/refs/tags/1.6.0...1.6.1
    
    Changelog: https://github.com/BLAKE3-team/BLAKE3/releases/tag/1.6.1

--- pkgs/applications/by-name/b3/b3sum/package.nix
+++ pkgs/applications/by-name/b3/b3sum/package.nix
@@ -5,20 +5,20 @@
 }:
 rustPlatform.buildRustPackage rec {
   pname = "b3sum";
-  version = "1.6.0";
+  version = "1.6.1";
 
   src = fetchFromGitHub {
     owner = "BLAKE3-team";
     repo = "BLAKE3";
     tag = version;
-    hash = "sha256-Ijen3IacwohI2tKxTWv0JWgRfiCQS/SDtkkqEUWiOzk=";
+    hash = "sha256-YJ3rRzpmF6oS8p377CEoRteARCD1lr/L7/fbN5poUXw=";
   };
 
   cargoRoot = "b3sum";
   buildAndTestSubdir = cargoRoot;
 
   useFetchCargoVendor = true;
-  cargoHash = "sha256-pF1mWu+syRD4vN07yMHMKW1Uv3viYeWpSn2fKMwjX5k=";
+  cargoHash = "sha256-eO0KgAuli2x8w4WHKZUK6D2KV7DmwiLKuX6FRKgNJOw=";
 
   meta = {
     description = "BLAKE3 cryptographic hash function";
